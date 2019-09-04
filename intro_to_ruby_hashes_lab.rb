75c52096b8be2304b06b96f44388eed5f75eb38e
Charles DeLeo <charles.deleo979@gmail.com>
2019-09-04 19:04:00 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,25 +1,25 @@
 def base_hash
   countries_and_capitals_of_the_world = {
-    :north_america => {
-      "Canada" => {
-        :capital => "Ottawa",
-        :capital_climate => "Kppen Dfb"
-      }, 
-      "USA" => {
-        :capital => "Washington D.C.",
-        :capital_climate => "Kőppen Cfa"
-      }
-    }, 
-    :africa => {
-      "Ghana" => {
-        :capital => "Accra",
-        :capital_climate => "Kőppen Aw"
-      },
-      "Nigeria" => {
-        :capital => "Abuja",
-        :capital_climate => "Kőppen Aw"
-      }
-    }
+    # :north_america => {
+    #   "Canada" => {
+    #     :capital => "Ottawa",
+    #     :capital_climate => "Kppen Dfb"
+    #   }, 
+    #   "USA" => {
+    #     :capital => "Washington D.C.",
+    #     :capital_climate => "Kőppen Cfa"
+    #   }
+    # }, 
+    # :africa => {
+    #   "Ghana" => {
+    #     :capital => "Accra",
+    #     :capital_climate => "Kőppen Aw"
+    #   },
+    #   "Nigeria" => {
+    #     :capital => "Abuja",
+    #     :capital_climate => "Kőppen Aw"
+    #   }
+    # }
   }
 end
 
